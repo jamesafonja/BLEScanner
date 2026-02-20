c373482aa091c1ba210260ddfa713683be5df2f6
James Afonja <97204146+jamesafonja@users.noreply.github.com>
2026-02-19 19:03:48 -0600

    Initial Commit

---
+++ BLEScanner/BLEScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScannerApp.swift
+//  BLEScanner
+//
+//  Created by SAfonja on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct BLEScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEScannerTests/BLEScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BLEScannerTests.swift
+//  BLEScannerTests
+//
+//  Created by SAfonja on 2/19/26.
+//
+
+import Testing
+@testable import BLEScanner
+
+struct BLEScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
